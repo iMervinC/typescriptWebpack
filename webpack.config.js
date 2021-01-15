783aa4e9f47488c4433ab7ded07163b3e59aab93
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-01-15 19:05:12 +0800

    d

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: './src/index.ts',
+  devServer: {
+    publicPath: '/',
+    contentBase: './public',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+}
